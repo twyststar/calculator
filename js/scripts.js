40efeb36a86928021165ddaff5de9f90b17adb32
Anatoliy Klots and Starlynne Start <starlynne@>
2017-03-20 11:20:36 -0700

    add initial files, js for add, subtract, multiply and divide

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var add = function(number1, number2) {
+ return number1 + number2;
+ };
+
+ var number1 = parseInt(prompt("Number 1:"));
+ var number2 = parseInt(prompt("Number 2:"));
+
+ alert(add(number1,number2));
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var mult = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide  = function(number1, number2) {
+  return number1 / number2;
+};
